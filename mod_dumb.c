f16f5fca1ec2d61139be4dda0636446873e49ea6
Javier Wilson <javier.wilson@gmail.com>
2011-02-08 15:30:34 +0100

    mod dumb

--- mod_dumb.c
+++ mod_dumb.c
@@ -1,4 +1,25 @@
-int mod_dumb(int adct[DLEN][DLEN]) {
+int dumb_hello_module()
+{
+  printf("dumb non-existant feature\n");
+  return 0;
+}
+
+int dumb_init(void)
+{
+  return 0;
+}
+
+int dumb_release(void)
+{
+  return 0;
+}
+
+int dumb_reset(void)
+{
+  return 0;
+}
+
+int dumb_compute(int adct[DLEN][DLEN]) {
   int x,y,z;
   int sum=0;
   for (x=0; x<DLEN; x++) {
@@ -6,5 +27,16 @@ int mod_dumb(int adct[DLEN][DLEN]) {
       sum += adct[x][y];
     }
   }
-  return sum;
+  return 0;
+}
+
+int dumb_get_features() {
+  printf("DCT # %d of %d\n", z+1, size);
+  for (x=0; x<DLEN; x++) {
+    for (y=0; y<DLEN; y++) {
+      printf("%4d ", dct[z][x][y]);
+    }
+    printf("\n");
+  }
+  return 0;
 }
